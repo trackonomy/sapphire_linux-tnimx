TechNexion/linux-tn-imx

PCI: imx7: set default PCI reference clock from internal clock instead of external clock

drivers/pci/dwc/pci-imx6.c
@@ -1304,7 +1304,7 @@ static void imx_pcie_init_phy(struct imx_pcie *imx_pcie)
 
 		/* pcie phy ref clock select; 1? internal pll : external osc */
 		regmap_update_bits(imx_pcie->iomuxc_gpr, IOMUXC_GPR12,
-				   IMX7D_GPR12_PCIE_PHY_REFCLK_SEL, 0);
+				   IMX7D_GPR12_PCIE_PHY_REFCLK_SEL, imx_pcie->ext_osc ? 0 : BIT(5));
 	} else if (imx_pcie->variant == IMX6SX) {
 		/* Force PCIe PHY reset */
 		regmap_update_bits(imx_pcie->iomuxc_gpr, IOMUXC_GPR5,

trackonomy/sapphire_linux-tnimx

drivers/pci/controller/dwc/pci-imx6.c
@@ -503,7 +503,7 @@ static void imx6_pcie_init_phy(struct imx6_pcie *imx6_pcie)
 	case IMX7D:
 	case IMX7D_EP:
 		regmap_update_bits(imx6_pcie->iomuxc_gpr, IOMUXC_GPR12,
-				   IMX7D_GPR12_PCIE_PHY_REFCLK_SEL, 0);
+				   IMX7D_GPR12_PCIE_PHY_REFCLK_SEL, imx6_pcie->ext_osc ? 0 : BIT(5));
 		break;
 	case IMX6SX:
 	case IMX6SX_EP: